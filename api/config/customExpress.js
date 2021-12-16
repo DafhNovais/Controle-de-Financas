aa42fb4160e2f34d15a61eda70303ced9e3cf6c5
anna <carolinanna93@gmail.com>
2021-12-15 23:42:40 -0300

    Configura o cors no express

--- api/config/customExpress.js
+++ api/config/customExpress.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 module.exports = () => {
     const app = express();
 
-    app.use(bodyParser.urlencoded({ extended: true }))
-    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        app.use(cors());
+        next();
+    });
 
     consign()
         .include('controllers')
